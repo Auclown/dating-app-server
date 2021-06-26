62124e3a61ff795226760e81fc6e37d4b251278c
Jim Park <auclown@gmail.com>
2021-06-26 18:22:46 +1200

    26th/Jun/2021 - API extended. Seeding user data, more Dtos.

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -6,7 +6,7 @@ namespace API.Entities
     public class Photo
     {
         public int Id { get; set; }
-        public string Url { get; set; }
+        public string url { get; set; }
         public bool isMain { get; set; }
         public string PublicId { get; set; }
         public AppUser AppUser { get; set; }
