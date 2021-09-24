389cc04460353905ab2123b82627227921f1260b
Auclown <auclown@gmail.com>
2021-09-24 23:40:18 +1200

    24th/Sep/2021 - implemented CreateToken

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Services;
+using API.Interfaces;
 
 namespace API
 {
@@ -27,6 +29,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
